bacd4ed90f2344f6dad97bb220811d94ae5def26
shakibaam <shakibaamirshahi@yahoo.com>
2021-12-01 19:21:35 +0330

    work on saving

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
-function formSubmit(){
+function formSubmit() {
 
     let radioButtons = document.getElementsByName("gender")
     let name = document.getElementById("name").value;
-    console.log(name)
-    let gender;
-    for(var i = 0; i < radioButtons.length; i++){
-        if(radioButtons[i].checked){
-            gender= radioButtons[i].value;
-        }
-    }
+    console.log(name);
 
     console.log("lets fetch")
-    fetch(`https://api.genderize.io/?name=${name}`,{method:"GET"})
+    fetch(`https://api.genderize.io/?name=${name}`, {method: "GET"})
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => {
 
+            console.log(data);
+            nameObj = JSON.stringify(data);
+            nameObj = JSON.parse(nameObj)
 
+            document.getElementById("gender-predict").innerHTML = nameObj.gender;
+            document.getElementById("prob-predict").innerHTML = nameObj.probability;
+            if (localStorage.getItem(name)) {
+                document.getElementById("saved-gender").innerHTML = localStorage.getItem(name);
+                document.getElementsByClassName("saved-answer")[0].style.visibility = "visible";
+            }
+
+        });
+
+    event.preventDefault()
+
+}
+
+function save() {
+    let name = document.getElementById("name").value;
+    let radioButtons = document.getElementsByName("gender");
+    console.log(name)
+    let gender = null;
+    for (var i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            gender = radioButtons[i].value;
+        }
+    }
+    if (gender != null) {
+        if (localStorage.getItem(name)) {
+            localStorage.removeItem(name);
+            localStorage.setItem(name, gender);
+        } else {
+            localStorage.setItem(name, gender)
+        }
+    } else {
+
+        localStorage.setItem(name, document.getElementById("gender-predict").innerHTML)
+    }
     event.preventDefault()
 
 }
